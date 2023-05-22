87052aac224fb40d4a9cf5eb87029e25574b457f
era <me@elias.sh>
2023-05-22 19:12:31 +0100

    add simple test for watch

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,11 @@
 use assert_cmd::prelude::*;
 use std::collections::HashMap;
 use std::fs;
+use std::fs::File;
 use std::path::Path;
 use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
 use tempdir::TempDir;
 
 fn read_folder_contents(folder_path: &Path) -> HashMap<String, String> {
@@ -61,4 +64,43 @@ fn test_creates_basic_output() {
         };
         assert_eq!(expected_content, &value);
     }
+}
+
+#[test]
+fn test_watches() {
+    let tempdir = TempDir::new("output").unwrap();
+    let static_folder = TempDir::new("static_folder").unwrap();
+
+    let output_folder = tempdir.path().to_str().to_owned().unwrap().to_string();
+    let static_folder_cmd = static_folder
+        .path()
+        .to_str()
+        .to_owned()
+        .unwrap()
+        .to_string();
+
+    //TODO stop leaking thread
+    std::thread::spawn(move || {
+        println!("aaa");
+        Command::cargo_bin("squid")
+            .unwrap()
+            .arg("--template-folder")
+            .arg("tests/templates")
+            .arg("--output-folder")
+            .arg(output_folder)
+            .arg("--markdown-folder")
+            .arg("tests/markdown")
+            .arg("--template-variables")
+            .arg("tests/config.toml")
+            .arg("--static-resources")
+            .arg(static_folder_cmd)
+            .arg("--watch")
+            .unwrap()
+            .unwrap();
+    });
+
+    File::create(static_folder.into_path().join("hello.txt")).unwrap();
+    //TODO avoid sleeping during tests
+    sleep(Duration::from_millis(100));
+    assert!(tempdir.into_path().join("hello.txt").exists())
 }
